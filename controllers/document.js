6341884fd169a7717facbd5cc79840dc939a87c3
Jonathan Veloso <velosojonathan5@gmail.com>
2019-03-08 00:49:13 -0300

    remove(apiDoc): removing documentation generated files from git

--- controllers/document.js
+++ controllers/document.js
@@ -2,7 +2,7 @@ const models = require('../db/models');
 exports.get = async (req, res) => {
     try{
         res.send({
-            documents: await models.Document.findAll()
+            documents: await models.Document.findAll({ where: { status: 1} })
         });
     }catch(err){
         console.log(err);
--- routes/document-type.js
+++ routes/document-type.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/document-type');
 
 /**
  * @api {get} /document-types DocumentTypes
- * @apiGroup Sistema
+ * @apiGroup DocumentType
  *
  * @apiSuccess {Array} DocumentTypes List of Document Types
  * 
@@ -12,31 +12,25 @@ const controller = require('../controllers/document-type');
  *    {
  *    "documentTypes": [
  *          {
- *              "id": 5,
+ *              "id": 1,
  *              "description": "Empresa",
  *              "createdAt": "2019-03-05T23:21:09.702Z",
  *              "updatedAt": "2019-03-05T23:21:09.702Z"
  *          },
  *          {
- *              "id": 6,
+ *              "id": 2,
  *              "description": "Colaborador",
  *              "createdAt": "2019-03-05T23:21:09.702Z",
  *              "updatedAt": "2019-03-05T23:21:09.702Z"
  *          },
  *          {
- *              "id": 7,
+ *              "id": 3,
  *              "description": "Mensais",
  *              "createdAt": "2019-03-05T23:21:09.702Z",
  *              "updatedAt": "2019-03-05T23:21:09.702Z"
- *          },
- *          {
- *              "id": 8,
- *              "description": "Mensais colaborador",
- *              "createdAt": "2019-03-05T23:21:09.702Z",
- *              "updatedAt": "2019-03-05T23:21:09.702Z"
  *          }
- *      ]
- *      }
+ *    ]
+ *    }
  */
 router.get('/', controller.get);
 
