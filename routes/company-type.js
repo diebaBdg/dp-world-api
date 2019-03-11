e1d110773216b10fe619677495f7b57d1a1930c2
Jonathan Veloso <velosojonathan5@gmail.com>
2019-03-11 01:07:54 -0300

    feat(CompanyType): method get

---
+++ routes/company-type.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const controller = require('../controllers/company-type');
+
+/**
+ * @api {get} /company-types List of company types
+ * @apiName GetCompanyTypes
+ * @apiGroup CompanyTypes
+ *
+ * @apiSuccess {Array} CompanyTypes List of company types
+ * 
+ * @apiSuccessExample {json} Success (example)
+ *    HTTP/1.1 200 OK
+ *    {
+ *       "CompanyTypes": [
+ *          {
+ *               "id": 1,
+ *               "description": "Estrangeiro",
+ *               "status": 1,
+ *               "createdAt": "2019-03-11T03:48:19.020Z",
+ *               "updatedAt": "2019-03-11T03:48:19.020Z"
+ *           },
+ *           {
+ *               "id": 2,
+ *               "description": "Fornecimento",
+ *               "status": 1,
+ *               "createdAt": "2019-03-11T03:48:19.020Z",
+ *               "updatedAt": "2019-03-11T03:48:19.020Z"
+ *           }
+ *       ]
+ *    }
+ */
+router.get('/', controller.get);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ exports.routes = (app) => {
     app.use('/documents', require('./document'));
     app.use('/functions', require('./function'));
     app.use('/document-types', require('./document-type'));
+    app.use('/company-types', require('./company-type'));
     return app;
 }
