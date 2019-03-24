dea5c7cd90fc7d00e4ac28202fefc767445c690e
Jonathan Veloso <velosojonathan5@gmail.com>
2019-03-24 01:10:03 -0300

    feat(Auth): create authorization components and login route

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ const validators = require('./validators/auth-validators');
  * @apiParam (Request body) {String} email User email
  * @apiParam (Request body) {String} password User password
  * 
- * @apiSuccess {Int} id Id of the user
+ * @apiSuccess {Int} token JWT access token
  * @apiSuccess {Object} user User data
  * 
  * @apiSuccessExample {json} Success (example):
