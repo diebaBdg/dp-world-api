d158b883e27ce1265042147ffb3f4401f640f7f0
Jonathan Veloso <velosojonathan5@gmail.com>
2019-03-25 00:52:12 -0300

    feat(Tests): organize tests

--- __tests__/api/company.js
+++ __tests__/api/company.js
@@ -1,7 +1,14 @@
 const frisby = require('frisby');
+frisby.globalSetup({
+    request: {
+        headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Mjh9.p7eo9A6pS9gHrM2YRUdyMUiLLqj1RqSNumpH8U_QR1s'
+        }
+    }
+});
 
 it('GET shoud return a list of companies', () => {
-    return frisby.get('http://localhost:3000/companies')
-        .expect('status', 200)
-        .expect('jsonTypes', 'data', frisby.Joi.array())
+        return frisby.get(`http://localhost/companies`)
+                .expect('status', 200)
+                .expect('jsonTypes', 'data', frisby.Joi.array())
 })
