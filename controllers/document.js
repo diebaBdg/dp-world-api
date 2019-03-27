65e992c7743bb7c8a7b0b08746c5b37127e5f55d
Jonathan Veloso <velosojonathan5@gmail.com>
2019-03-26 23:17:24 -0300

    refactor(Document): change existis validate to validation file

--- controllers/document.js
+++ controllers/document.js
@@ -1,6 +1,6 @@
 const models = require('../db/models');
 exports.get = async (req, res) => {
-    try{
+    try {
         // select filters
         let filters = req.query;
         filters.status = 1;
@@ -8,27 +8,20 @@ exports.get = async (req, res) => {
         res.send({
             data: await models.Document.findAll({ where: filters })
         });
-    }catch(err){
+    } catch (err) {
         console.log(err);
-        res.status(500).send({msg: 'Internal Error'})
+        res.status(500).send({ msg: 'Internal Error' })
     }
 }
 
 exports.post = async (req, res) => {
-    try{
+    try {
         // get request body
         let document = req.body;
-        // get documents and verify if exists document with the same name
-        const documents = await models.Document.findAll({ where: { description: req.body.description } });
-        if(!documents.length){
-            // set status active and creating document
-            document.status = 1;
-            res.status(201).send({id: (await models.Document.create(document)).id});
-        }else{
-            res.status(400).send({ msg: "Item already exists." });
-        }
-    }catch(err){
+        document.status = 1;
+        res.status(201).send({ id: (await models.Document.create(document)).id });
+    } catch (err) {
         console.log(err);
-        res.status(500).send({msg: 'Internal Error'})
+        res.status(500).send({ msg: 'Internal Error' })
     }
 }
--- routes/validators/document-validators.js
+++ routes/validators/document-validators.js
@@ -1,4 +1,5 @@
 const { check, validationResult } = require('express-validator/check');
+const models = require('../../db/models');
 
 // specific validator of company routes
 exports.get = [
@@ -7,7 +8,21 @@ exports.get = [
 ];
 
 exports.post = [
-    check('description').isLength({min:3, max: 200}).withMessage("Should be between 3 and 200 characters."),
-    check('DocumentTypeId').isInt().withMessage("Should be an integer."),
-    check('FunctionId').optional({nullable: true}).isInt().withMessage("Should be an integer.")
+    check('description')
+        .isLength({min:3, max: 200})
+        .withMessage("Should be between 3 and 200 characters.")
+        .custom((description)=>{
+            return models.Document.findOne({ where: { description: description } }).then(document => {
+                if (document) {
+                    return Promise.reject('Já existe documento com essa descrição.');
+                }
+            });
+        }),
+    check('DocumentTypeId')
+        .isInt()
+        .withMessage("Should be an integer."),
+    check('FunctionId')
+        .optional({nullable: true})
+        .isInt()
+        .withMessage("Should be an integer.")
 ];
