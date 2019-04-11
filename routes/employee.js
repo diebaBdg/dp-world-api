2994c6ea13040ab1e7dabad4e3aee781073f4de0
Jonathan Veloso <velosojonathan5@gmail.com>
2019-04-11 03:16:09 -0300

    feat(Employee): POST

--- routes/employee.js
+++ routes/employee.js
@@ -29,4 +29,40 @@ router.use(auth.authenticate());
  */
 router.get('/', validators.get, expressValidator.findsValidatorErros(), controller.get);
 
+/**
+ * @api {post} /employees Create a employee
+ * @apiName PostEmployees
+ * @apiGroup Employees
+ * 
+ * @apiParam (Request body) {String} name Employee name.
+ * @apiParam (Request body) {Date} birthDate The birth date. format: YYYY-MM-DD
+ * @apiParam (Request body) {String} sector The description about the employee sector.
+ * @apiParam (Request body) {String} rg Brazilian personal document.
+ * @apiParam (Request body) {String} cpf Brazilian personal document number.
+ * @apiParam (Request body) {String} phone The phone number.
+ * @apiParam (Request body) {String} email The employee email.
+ * @apiParam (Request body) {String} address The local of the company.
+ * @apiParam (Request body) {String} number The street number.
+ * @apiParam (Request body) {String} complement The complement address.
+ * @apiParam (Request body) {String} district The district.
+ * @apiParam (Request body) {String} city The city.
+ * @apiParam (Request body) {String} state The state or province.
+ * @apiParam (Request body) {String} country The country.
+ * @apiParam (Request body) {String} cep The postal code.
+ * @apiParam (Request body) {Int} CompanyId The company id.
+ * @apiParam (Request body) {Int} FunctionId The function id.
+ *
+ * @apiSuccess {Int} id Function inserted
+ * @apiSuccess {String} msg Success message
+ * 
+ * @apiSuccessExample {json} Success (example)
+ *    HTTP/1.1 200 OK
+ *    {
+ *        "id": 17,
+ *        "msg": "Cadastrado com sucesso."
+ *    }
+ */
+router.post('/', validators.post, expressValidator.findsValidatorErros(), controller.post);
+
+
 module.exports = router;
--- routes/validators/employee-validators.js
+++ routes/validators/employee-validators.js
@@ -1,4 +1,5 @@
 const { check, validationResult } = require('express-validator/check');
+const models = require('../../db/models');
 
 // specific validator of company routes
 exports.get = [
@@ -18,4 +19,78 @@ exports.get = [
         .optional()
         .isIn(['ASC', 'DESC'])
         .withMessage("Deve ser ASC ou DESC.")
+];
+
+exports.post = [
+    check('name')
+        .isLength({ min: 3, max: 50 })
+        .withMessage("Deve ter entre 3 e 50 caracteres."),
+    check('birthDate')
+        .matches(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)
+        .withMessage("Deve ser uma data no formato YYYY-MM-DD."),
+    check('cpf')
+        .isNumeric()
+        .withMessage("Deve ser um numérico de 11 caracteres.")
+        .isLength({ min: 11, max: 11 })
+        .withMessage("Deve ser um numérico de 11 caracteres.")
+        .custom((cpf) => {
+            return models.Employee.findOne({ where: { cpf: cpf } }).then(employee => {
+                if (employee) {
+                    return Promise.reject('CPF já em uso.');
+                }
+            });
+        }),
+    check('sector')
+        .isLength({ min: 3, max: 50 })
+        .withMessage("Deve ter entre 3 e 50 caracteres."),
+    check('rg')
+        .isAlphanumeric()
+        .withMessage("Deve ser alfanumérico."),
+    check('phone')
+        .isNumeric()
+        .isLength({ min: 10, max: 11 })
+        .withMessage("Deve ter 10 ou 11 caracteres."),
+    check('email')
+        .isEmail()
+        .withMessage("Deve ser um email válido")
+        .custom((email) => {
+            return models.Employee.findOne({ where: { email: email } }).then(employee => {
+                if (employee) {
+                    return Promise.reject('Email já em uso.');
+                }
+            });
+        }),
+    check('address')
+        .isLength({ min: 3, max: 200 })
+        .withMessage("Deve ter entre 3 e 200 caracteres."),
+    check('number')
+        .isLength({ min: 1, max: 20 })
+        .withMessage("Deve ter entre 1 e 20 caracteres"),
+    check('complement')
+        .optional({ nullable: true })
+        .isLength({ min: 3, max: 200 })
+        .withMessage("Deve ter entre 3 e 200 caracteres."),
+    check('district')
+        .isLength({ min: 3, max: 200 })
+        .withMessage("Deve ter entre 3 e 200 caracteres."),
+    check('city')
+        .isLength({ min: 3, max: 200 })
+        .withMessage("Deve ter entre 3 e 200 caracteres."),
+    check('state')
+        .isLength({ min: 2, max: 2 })
+        .withMessage("Should be 2 characters."),
+    check('country')
+        .isLength({ min: 3, max: 200 })
+        .withMessage("Deve ter entre 3 e 200 caracteres."),
+    check('cep')
+        .optional({ nullable: true })
+        .isNumeric()
+        .isLength({ min: 8, max: 8 })
+        .withMessage("Deve ter 8 caracteres"),
+    check('CompanyId')
+        .isInt()
+        .withMessage("Deve ser um número inteiro."),
+    check('FunctionId')
+        .isInt()
+        .withMessage("Deve ser um número inteiro.")
 ];
