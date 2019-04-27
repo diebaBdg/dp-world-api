2128d1bdb17c1dc0bab3deec96ffc4fbbca7dd74
Jonathan Veloso <velosojonathan5@gmail.com>
2019-04-27 17:52:24 -0300

    feat(Notification): migration, model, seed and method GET

---
+++ controllers/notification.js
@@ -0,0 +1,38 @@
+const models = require('../db/models');
+const Paginator = require('../helpers/paginator-helper');
+const orderHerper = require('../helpers/order-helper');
+
+exports.get = async (req, res) => {
+    try {
+        const paginator = new Paginator(req.query.page);
+        // filter definition
+        let filter = {};
+        if (req.query.UserId) {
+            filter.UserId = req.query.UserId
+        }
+        if (req.query.EmployeeId) {
+            filter.EmployeeId = req.query.EmployeeId
+        }
+        if (req.query.visualized) {
+            filter.visualized = req.query.visualized
+        }
+        let data = await models.Notification.findAndCountAll({
+            where: filter,
+            include: [{
+                model: models.User,
+                attributes: ['id', 'name']
+            },{
+                model: models.Employee,
+                attributes: ['id', 'name']
+            }],
+            order: orderHerper.getOrder(req.query.order_by, req.query.order_direction),
+            limit: paginator.limit,
+            offset: paginator.offset
+        });
+        data.pages = paginator.getNumberOfPages(data.count);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ msg: 'Internal Error' })
+    }
+}
