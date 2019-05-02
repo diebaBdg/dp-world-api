e1654c1a9650e5e156ec866cb47ef1574bd517a9
Jonathan Veloso <velosojonathan5@gmail.com>
2019-05-02 11:40:50 -0300

    feat(Auth): validator

--- routes/validators/auth-validators.js
+++ routes/validators/auth-validators.js
@@ -2,9 +2,9 @@ const { check, validationResult } = require('express-validator/check');
 
 // specific validator of company routes
 exports.post = [
-    // check('email')
-    //     .isEmail()
-    //     .withMessage("Deve ser um email"),
+    check('email')
+        .isEmail()
+        .withMessage("Deve ser um email"),
     check('password')
         .isLength({min:3, max: 50})
         .withMessage("Deve ter entre 3 e 50 caracteres.")
